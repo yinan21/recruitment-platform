02d9beebe03d78408f241833f774810f8ad9fed0
Yinan Zhang <yinan.zhang@gimo.co.uk>
2026-04-20 08:45:15 +0100

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
     ],
     build: {
         outDir: 'public/build',
-        manifest: true,
+        manifest: 'manifest.json', // Change true to 'manifest.json'
     },
 });
